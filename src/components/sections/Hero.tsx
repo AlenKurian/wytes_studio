50d3806a7ef7a15be141589ecd040752aa8040db
Alen Kurian <alenkurian001@gmail.com>
2026-08-24 11:05:27 +0530

    updateessss

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -31,7 +31,7 @@ export function Hero() {
           fill
           priority
           sizes="100vw"
-          className="object-cover"
+          className="object-cover object-[75%_50%] md:object-center"
         />
       </div>
 
