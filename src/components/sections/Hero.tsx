d16951e631c00e097639d6fe8da1f124d298cf44
Alen Kurian <alenkurian001@gmail.com>
2026-09-07 14:30:55 +0530

    wyteupdated

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -31,7 +31,7 @@ export function Hero() {
           fill
           priority
           sizes="100vw"
-          className="object-cover"
+          className="object-cover scale-90"
         />
       </div>
 
